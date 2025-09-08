8e5b6b34f26b6fe2f357a131dfe7b278685f3a77
hogcycle <nate@gysli.ng>
2025-09-07 20:39:02 -0400

    Finished task 4: html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import React from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import "./App.css";
-
+import image from "./assets/image.jpg";
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>Hi, my name is Nathaniel Gysling.</p>
-
-            <p> Hello World</p>
-
+            <h1 className="banner" style={{ background: "blue" }}>
+                <span style={{ color: "blue" }}>CISC275</span>
+            </h1>
+            <h1 role="banner">Hello Shooters</h1>
+            <img src={image} alt="up Connector"></img>
+            <ul>
+                <li>Use</li>
+                <li>Your</li>
+                <li>Imagination</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <div style={{ backgroundColor: "red", width: 500, height: 50 }}>
+                <Container>
+                    <Row>
+                        <Col>Terrible</Col>
+                        <Col>Excellence</Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
